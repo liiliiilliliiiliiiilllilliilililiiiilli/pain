8c808a7c5a790dcabdac971c5834656875252ae8
li <86910127+i-rick-y@users.noreply.github.com>
2025-04-23 02:27:40 +0300

    something

--- something.py
+++ something.py
@@ -6,6 +6,7 @@ from aiogram import Bot, Dispatcher, html
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
+from aiogram import types
 from aiogram.types import Message
 
 
