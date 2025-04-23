902b26269d3aac61015f0e17f7792ade5307aeda
li <86910127+i-rick-y@users.noreply.github.com>
2025-04-23 19:20:03 +0300

    something

--- something.py
+++ something.py
@@ -318,7 +318,8 @@ keyboard_markup_main = types.ReplyKeyboardMarkup (
     keyboard = [[
         types.KeyboardButton (text = texts['from_user']['help_me_please'])
     ]],
-    resize_keyboard = True
+    resize_keyboard = True,
+    input_field_placeholder = 'k#(#₽/&3'
 
 )
 
