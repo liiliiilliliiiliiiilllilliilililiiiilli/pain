4b6d1fe3cff6371dae818c7342bcebd46e07905d
li <86910127+i-rick-y@users.noreply.github.com>
2025-04-23 17:39:31 +0300

    something

--- something.py
+++ something.py
@@ -3,6 +3,9 @@ import sys
 import random
 import redis
 
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+
 from aiogram import Bot, Dispatcher, html
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
@@ -26,6 +29,14 @@ texts_russian = {
         'i_could_try_to_help_you': 'Я постараюсь поддержать, если тебе больно. Просто попроси.',
 
         'what_is_this_bot_about': 'Я стараюсь, как могу, чтобы хоть в какой-то степени поддержать тех, кому больно. Напиши, и я отвечу. ',
+
+        'choose_a_language': 'Выберите язык',
+
+        'russian': 'Русский',
+
+        'english': 'Английский',
+
+        'chinese': 'Китайский',
 
         'i_could_try_to_help_you': 'Поддержу',
 
@@ -94,6 +105,13 @@ texts_russian = {
 
         'go_back': 'Назад'
 
+    },
+
+    menu: {
+
+        '/about': 'О боте',
+        '/settings': 'Настройки'
+
     }
 
 }
@@ -108,6 +126,15 @@ texts_english = {
         'i_could_try_to_help_you': 'I\'ll try to support you if you\'re in pain. Just ask.',
 
         'what_is_this_bot_about': 'I try my best to at least support those who are hurting. Write and I will answer.',
+
+        'choose_a_language': 'Choose a language',
+
+        'russian': 'Russian',
+
+        'english': 'English',
+
+        'chinese': 'Chinese',
+
 
         'i_could_try_to_help_you': 'I will support',
 
@@ -174,6 +201,12 @@ texts_english = {
         'choose_bot_language': 'Choose bot language',
         'go_back': 'Go back'
 
+    },
+
+    menu: {
+
+        '/about': 'About this bot',
+        '/settings': 'Settings'
     }
 
 }
@@ -190,6 +223,14 @@ texts_chinese = {
         'what_is_this_bot_about': '我尽力去支持那些正在受伤的人。写下来，我会回复。',
 
         'i_could_try_to_help_you': '我会支持',
+
+        'choose_a_language': '选择语言',
+
+        'russian': '俄语',
+
+        'english': '英语',
+
+        'chinese': '中国',
 
         'settings_choose_a_button': '选择按钮',
 
@@ -232,6 +273,12 @@ texts_chinese = {
         'choose_bot_language': '选择机器人语言',
         'go_back': '回去'
 
+    },
+
+    'menu': {
+
+        '/about': '关于这个机器人',
+        '/settings': '设置'
     }
 
 }
@@ -246,6 +293,22 @@ def something ():
 
     return random.choice (texts['from_bot']['help_normal_letters'])
 
+
+
+
+class Form (StatesGroup):
+
+    page_main = State ()
+    page_settings = State ()
+    page_settings_languages = State ()
+
+
+bot_menu = [
+
+    types.BotCommand (command = '/about', description = texts['menu']['about']),
+    types.BotCommand (command = '/settings', description = texts['menu']['settings'])
+
+]
 
 
 keyboard_markup_main = types.ReplyKeyboardMarkup (
@@ -271,6 +334,20 @@ keyboard_markup_settings = types.ReplyKeyboardMarkup (
 )
 
 
+keyboard_markup_settings_language = types.ReplyKeyboardMarkup (
+
+    keyboard = [
+        [types.KeyboardButton (text = texts['from_bot']['russian'])]
+        [types.KeyboardButton (text = texts['from_bot']['english'])],
+        [types.KeyboardButton (text = texts['from_bot']['chinese'])]
+    ],
+    resize_keyboard = True,
+    input_field_placeholder = texts['from_bot']['settings_choose_a_button']
+
+)
+
+
+
 
 bot = Bot (token = bot_token, default = DefaultBotProperties (parse_mode = ParseMode.HTML))
 
@@ -280,7 +357,15 @@ dp = Dispatcher ()
 @dp.message (CommandStart ())
 async def command_start_handler (message: Message):
 
-    await message.answer (texts['from_bot']['greeting'], reply_markup = keyboard_markup_main)
+    await state.set_state (Form.page_main)
+
+    await bot.set_my_commands (bot_menu)
+    await message.answer (
+
+        texts['from_bot']['greeting'],
+        reply_markup = keyboard_markup_main
+
+    )
 
 
 @dp.message ()
@@ -288,37 +373,100 @@ async def got_message (message: Message):
 
     if message.text == texts['from_user']['help_me_please']:
 
+        await state.set_state (Form.main)
         await message.answer (something ())
 
     elif message.text == texts['from_user']['commands']['about_this_bot']:
 
+        await state.set_state (Form.page_main)
         await message.answer (texts['from_bot']['what_is_this_bot_about'])
         
     elif message.text == texts['from_user']['commands']['settings']:
 
-        await message.answer (texts['from_bot']['settings_choose_a_button'], reply_markup = keyboard_markup_main)
+        await state.set_state (Form.page_settings)
+
+        await message.answer (
+
+            texts['from_bot']['settings_choose_a_button'],
+            reply_markup = keyboard_markup_settings
+
+        )
 
     else:
 
+        await state.set_state (Form.page_main)
         await message.answer (texts['from_bot']['i_could_try_to_help_you_if_you_ask'])
+
+
+@dp.message (Form.page_settings)
+async def settings_page_handler (message: Message):
+
+    if message.text == texts['from_user']['choose_bot_language']:
+
+        await state.set_state (Form.page_settings_languages)
+
+        await message.answer (
+
+            texts['from_bot']['choose_a_language'],
+            reply_markup = keyboard_markup_settings_language
+
+        )
+
+    elif message.text == texts['from_user']['go_back']:
+
+        await command_start_handler (message)
+
+
+@dp.message (Form.page_settings_languages)
+async def settings_language_page_handler (message: Message):
+
+    if message.text == texts['from_bot']['russian']:
+
+        texts = texts_russian
+
+        await state.set_state (Form.page_main)
+
+        await message.answer (
+
+            texts['from_bot']['setted_language_russian'],
+            reply_markup = keyboard_markup_main
+
+        )
+
+    elif message.text == texts['from_bot']['english']:
+
+        texts = texts_english
+
+        await state.set_state (Form.page_main)
+
+        await message.answer (
+
+            texts['from_bot']['setted_language_english'],
+            reply_markup = keyboard_markup_main
+        
+        )
+
+    elif message.text == texts['from_bot']['chinese']:
+
+        texts = texts_chinese
+
+        await state.set_state (Form.page_main)
+
+        await message.answer (
+
+            texts['from_bot']['setted_language_chinese'],
+            reply_markup = keyboard_markup_main
+
+        )
 
 
 
 
 async def main ():
 
-    bot_commands = [
-
-        types.BotCommand (command = '/about_this_bot', description = 'Об этом боте'),
-        types.BotCommand (command = '/settings', description = 'настройки бота')
-        # types.BotCommand (command = '/wipe_chat', description = 'удалить все и начать заново')
-
-    ]
-
-    await bot.set_my_commands (bot_commands)
-
-
     await dp.start_polling (bot)
+
+
 
 
 if __name__ == '__main__':
