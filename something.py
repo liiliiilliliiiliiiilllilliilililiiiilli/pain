325ed0a83036adad4eb7c63730b818653953f4c6
li <86910127+i-rick-y@users.noreply.github.com>
2025-04-26 15:19:07 +0300

    something

--- something.py
+++ something.py
@@ -612,7 +612,7 @@ async def command_about (message: Message, state: FSMContext):
     await message.answer (
 
         await texts (lambda texts: texts['from_bot']['what_is_this_bot_about'], message.from_user.id),
-        reply_markup = {remove_keyboard: true}
+        reply_markup = types.ReplyKeyboardRemove ()
 
     )
 
