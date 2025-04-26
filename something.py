e359195de715380f80cbdaa87e58c4d025c9485a
li <86910127+i-rick-y@users.noreply.github.com>
2025-04-26 19:39:36 +0300

    something

--- something.py
+++ something.py
@@ -536,7 +536,7 @@ async def bot_menu ():
             description = 'Старт'),
         types.BotCommand (
             command = '/about',
-            description = '/О боте'),
+            description = 'О боте'),
         types.BotCommand (
             command = '/settings',
             description = 'Настройки')
