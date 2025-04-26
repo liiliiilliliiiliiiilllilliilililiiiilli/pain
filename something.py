d2e17d689f58c7c803d37d45a468ba6837a5b623
li <86910127+i-rick-y@users.noreply.github.com>
2025-04-26 14:48:13 +0300

    something

--- something.py
+++ something.py
@@ -555,7 +555,7 @@ async def inline_keyboard_markup_about (message):
 
         inline_keyboard = [[
             types.InlineKeyboardButton (
-                texts = await texts (lambda texts: texts['from_bot']['our_channel'], message.from_user.id),
+                text = await texts (lambda texts: texts['from_bot']['our_channel'], message.from_user.id),
                 url = 'https://t.me/li_ta_mi')]
         ]
 
